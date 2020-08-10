5548a309130e29b8bda3b5bb58f101a34c64e963
endysuryo <endysuryalaksana@gmail.com>
2020-08-10 21:50:52 +0700

    Add token sign after login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from '../account/account.entity';
 import { JwtModule } from '@nestjs/jwt';
-// import { AuthService } from './auth.service';
-// import { Auth } from './auth.entity';
+import dotenv = require('dotenv');
+
+const { parsed } = dotenv.config({
+  path:
+    process.cwd() +
+    '/.env' +
+    (process.env.NODE_ENV ? '.' + process.env.NODE_ENV : ''),
+});
+process.env = { ...process.env, ...parsed };
 
 @Module({
   imports: [
